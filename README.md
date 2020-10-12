ba1237a2419d0321f44930a09c4d3028c1abcf8c
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2020-10-12 09:46:55 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Mongodb Cron Backup
+
+Run schedule to create full backup for mongodb database, save it on server and send copy to mail
+
+# Steps to install
+ 1. Rename **.env.example** to **.env** and update values
+ 2. Install packages via terminal **npm install**
+
+# Run via CLI
+```
+node index.js
+```
+
+## Anyone welcome to contact me anytime
+### My Website :- [mrabdelrahman10.com](http://bit.ly/WebMr10)
+### My Linkedin :- [linkedin.com/in/mrabdelrahman10](http://bit.ly/LiMr10)
---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const execShPromise = require("exec-sh").promise;
+
+const date = new Date();
+const folder_name = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}/${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`;
+
+(async () => {
+    let is_running = false;
+    setInterval(async () => {
+
+        if (is_running) {
+            console.log('[task]', new Date(), 'another task is already running');
+            return;
+        }
+        console.log('[task]', new Date(), 'Task starts to run');
+        is_running = true;
+
+        // dump the result to a folder
+        await execShPromise(`mkdir -p ${process.env.BASE_DIR}${folder_name}`).catch(() => console.log('[task.error]', 'in:', `mkdir -p ${folder_name}`));
+        await execShPromise(`cd ${process.env.BASE_DIR}${folder_name}`).catch(() => console.log('[task.error]', 'in:', `cd ${folder_name}`));
+        await execShPromise(`mongodump --db=${process.env.MONGO_DB_DATABASE} --port=${process.env.MONGO_DB_PORT} --username=${process.env.MONGO_DB_USER} --password=${process.env.MONGO_DB_PASSWORD} --authenticationDatabase=${process.env.MONGO_DB_AUTH} --out=${process.env.MONGO_DB_DATABASE}`).catch(() => console.log('[task.error]', 'in:', 'mongodump'));
+
+        console.log('[task]', new Date(), 'Backup folder is:-', folder_name);
+
+        is_running = false;
+        console.log('[task]', new Date(), 'Task is finished');
+
+    }, parseInt(process.env.RUNNING_EVERY || 1) * 1000 * 60 * 60)
+
+})();
